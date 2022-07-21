06e377b3ac18d462519c6de6106ed5e848c25f29
Shubham Barore <shubham07barore@gmail.com>
2022-07-21 13:16:33 +0530

    embed video

--- src/components/MainRoutes.jsx
+++ src/components/MainRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Homepage from "../pages/Homepage";
 import Singapore from "../pages/Singapore";
+import Dubai from "../pages/Dubai";
 
 // import Homepage from "../pages/Homepage";
 import { LandingPage } from "../pages/LandingPage";
@@ -11,6 +12,8 @@ const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/collections/dubai" element={<Dubai />} />
+      <Route path="/collections/singapore" element={<Singapore />} />
     </Routes>
   );
 };
