65a68148174dc5dcaf639b4194a1a8a45a3b8a78
Rohit gupta <rg15697@gmail.com>
2022-07-22 17:23:40 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Footer from './components/Footer/Footer';
 import MainRoutes from './components/MainRoutes';
 import Navbar from './components/Navbar/Navbar';
 import Packages from './pages/Packages';
+import ProceedToPaymentPage from './pages/ProceedToPaymentPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <MainRoutes />
-      <Packages />
+      {/* <MainRoutes />*/}
+    <ProceedToPaymentPage />
+   
       <Footer />
     </div>
   );
